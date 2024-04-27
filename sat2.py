a1c3db3c64801c292e040de3e47b12e3dfe853d8
Richard Körösi <korosi.richard@gmail.com>
2024-04-27 12:18:22 +0200

    Update sat2.py

--- sat2.py
+++ sat2.py
@@ -1,25 +1,29 @@
 def read_file(file_name):
-    clausules = []
-    no_lit = 0
+    clauses = []
     with open(file_name, 'r') as f:
         lines = f.readlines()
 
     for line in lines:
-        clausules.append(line.split())
+        clauses.append(line.split())
 
+    no_var = clauses[0][0]
 
-    no_lit = clausules[0][0]
+    for i in range(len(clauses)):
+        clauses[i] = clauses[i][:-1]
 
-    for i in range(len(clausules)):
-        clausules[i] = clausules[i][:-1]
-
-    return clausules[1:], no_lit
-
+    return clauses[1:], no_var
 
 
 def main():
-    clausles, no_lit = read_file('sat2.txt')
-    print(clausles, no_lit)
+    clauses, no_var = read_file('sat2.txt')
+    graph = {}
+    for i in range(1, int(no_var) + 1):
+        graph[i] = []
+        graph[-i] = []
+    for i, j in clauses:
+        graph[-int(i)].append(int(j))
+        graph[-int(j)].append(int(i))
+    return
 
 
 if __name__ == '__main__':
