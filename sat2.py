b663fade2fa78b8ec4a216ebdd34f3d74b43d8f8
Richard Körösi <korosi.richard@gmail.com>
2024-04-27 12:48:54 +0200

    Update sat2.py

--- sat2.py
+++ sat2.py
@@ -17,13 +17,64 @@ def read_file(file_name):
 def main():
     clauses, no_var = read_file('sat2.txt')
     graph = {}
+
     for i in range(1, int(no_var) + 1):
         graph[i] = []
         graph[-i] = []
-    for i, j in clauses:
-        graph[-int(i)].append(int(j))
-        graph[-int(j)].append(int(i))
-    return
+    for clause in clauses:
+        if len(clause) == 2:
+            i, j = clause
+            graph[-int(i)].append(int(j))
+            graph[-int(j)].append(int(i))
+        elif len(clause) == 1:
+            i = clause[0]
+            graph[-int(i)].append(int(i))
+    ssc_map = tarjan(graph)
+    for i in range(1, int(no_var) + 1):
+        if ssc_map[i] == ssc_map[-i]:
+            print('UNSATISFIABLE')
+            return False
+    print('SATISFIABLE')
+    return True
+
+
+def tarjan(graph):
+    index = 1
+    stack = []
+    ssc_map = {}
+    low_link = {}
+    index_map = {}
+    for key in graph.keys():
+        ssc_map[key] = 0
+        low_link[key] = 0
+        index_map[key] = 0
+
+    for key in graph.keys():
+        if index_map[key] == 0:
+            index = strong_connect(graph, key, index, stack, ssc_map, low_link, index_map)
+    return ssc_map
+
+
+def strong_connect(graph, key, index, stack, ssc_map, low_link, index_map):
+    index_map[key] = index
+    low_link[key] = index
+    index += 1
+    stack.append(key)
+
+    for neighbor in graph[key]:
+        if index_map[neighbor] == 0:
+            index = strong_connect(graph, neighbor, index, stack, ssc_map, low_link, index_map)
+            low_link[key] = min(low_link[key], low_link[neighbor])
+        elif ssc_map[neighbor] == 0:
+            low_link[key] = min(low_link[key], index_map[neighbor])
+
+    if low_link[key] == index_map[key]:
+        while True:
+            node = stack.pop()
+            ssc_map[node] = low_link[key]
+            if node == key:
+                break
+    return index
 
 
 if __name__ == '__main__':
